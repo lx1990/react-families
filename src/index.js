1ff1fadecac8fcb9afa50d3b06009b6ff1f65fa9
lx1990 <lx9999@163.com>
2019-05-10 00:47:22 +0800

    使用redux管理数据

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 
 // import App from './App'; //original
 // import App from './steps/1/Conut'//最简单的计数器
-import App from './steps/2/Parent'// 回调传递信息
+// import App from './steps/2/Parent'// 回调传递信息
+import App from './steps/3/Count'// redux管理数据
 ReactDOM.render(
   <App />,
   document.getElementById('root')
