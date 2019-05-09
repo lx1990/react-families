d244469ed9ec018ac2ad688a5d296f3f6bf541a2
lx1990 <lx9999@163.com>
2019-05-09 23:45:20 +0800

    最简单的计数器

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
+// import App from './App'; //original
+import App from './steps/1/Conut'//最简单的计数器
 ReactDOM.render(
   <App />,
   document.getElementById('root')
