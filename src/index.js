0ed688a09bfa334f4da82f3f18bc4f744155de1a
lx1990 <lx9999@163.com>
2019-05-10 18:44:48 +0800

    引入react-redux管理跨组件管理数据

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import './index.css';
 // import App from './steps/1/Conut'//最简单的计数器
 // import App from './steps/2/Parent'// 回调传递信息
 // import App from './steps/3/Count'// redux管理数据
-import App from './steps/4/Parent'; // redux跨组件管理数据
+// import App from './steps/4/Parent'; // redux跨组件管理数据
+import App from './steps/5/index'; // 引入react-redux管理跨组件管理数据
+// import App from './steps/6/index'; // 引入redux-thunk管理异步数据派发
+// import App from './steps/7/index'; // 路由+react-redux + redux + redux-thunk
 ReactDOM.render(
     <App/>,
     document.getElementById('root')
