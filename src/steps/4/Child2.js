a0c13b3afd8311b3e35e6d5d80dd00c5528294fc
lx1990 <lx9999@163.com>
2019-05-10 12:17:43 +0800

    redux跨组件管理数据

---
+++ src/steps/4/Child2.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export default ({add2Action,store,updateState}) => <button onClick={() => {
+    store.dispatch(add2Action);updateState()
+}}>chid2--左边点击{store.getState().child1}次，总共{store.getState().count}次</button>
